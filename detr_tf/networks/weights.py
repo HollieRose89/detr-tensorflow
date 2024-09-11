2a91b33e1faea2aa86bc669218a0721137864e23
Hollie Rose <hollyrose0209@outlook.com>
2024-09-11 12:10:12 +0800

    Update weights.py

--- detr_tf/networks/weights.py
+++ detr_tf/networks/weights.py
@@ -12,34 +12,27 @@ WEIGHT_NAME_TO_CKPT = {
 
 def load_weights(model, weights: str):
     """ Load weight on a given model
-    Weights are supposed to be stored in the weight folder at the root of the repository. If weights
-    do not exist, but are publicly known, the weight will be downloaded from gcloud.
+    weights are supposed to be sotred in the weight folder at the root of the repository. If weights
+    does not exists, but are publicly known, the weight will be download from gcloud.
     """
     if not os.path.exists('weights'):
         os.makedirs('weights')
     
-    if weights.endswith('.ckpt'):
-        # Convert .ckpt to .h5 or .keras format if needed
-        ckpt_path = os.path.join('weights', weights)
-        if not os.path.exists(ckpt_path):
-            os.makedirs(ckpt_path)
+    if "ckpt" in "weights":
+        model.load(weights)
+    elif weights in WEIGHT_NAME_TO_CKPT:
+        wdir = f"weights/{weights}"
+        if not os.path.exists(wdir):
+            os.makedirs(wdir)
         for f in WEIGHT_NAME_TO_CKPT[weights]:
             fname = f.split("/")[-1]
-            if not os.path.exists(os.path.join(ckpt_path, fname)):
-                print("Downloading...", f)
+            if not os.path.exists(os.path.join(wdir, fname)):
+                print("Download....", f)
                 r = requests.get(f, allow_redirects=True)
-                open(os.path.join(ckpt_path, fname), 'wb').write(r.content)
-        print("Restoring weights from", os.path.join(ckpt_path, f"{weights}"))
-        checkpoint = tf.train.Checkpoint(model=model)
-        checkpoint.restore(os.path.join(ckpt_path, f"{weights}")).expect_partial()
-    elif weights.endswith('.h5') or weights.endswith('.keras'):
-        # Directly load .h5 or .keras files
-        weight_path = os.path.join('weights', weights)
-        if not os.path.exists(weight_path):
-            print("Downloading...", weights)
-            r = requests.get(weight_path, allow_redirects=True)
-            open(weight_path, 'wb').write(r.content)
-        print("Loading weights from", weight_path)
-        model.load_weights(weight_path)
+                open(os.path.join(wdir, fname), 'wb').write(r.content)
+        print("Load weights from", os.path.join(wdir, f"{weights}.ckpt"))
+        l = model.load_weights(os.path.join(wdir, f"{weights}.keras"))
+        l.expect_partial()
     else:
-        raise Exception(f'Cannot load the weights: {weights}')
+        raise Exception(f'Cant load the weights: {weights}')
+   
